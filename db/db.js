4c802133b2148e993c12c1dd21a5f3366ced3af9
Rafi5734 <kamrulhasun5734@gmail.com>
2023-04-15 22:51:21 +0600

    error from htp request unhandle for send multiple response from one event

--- db/db.js
+++ db/db.js
@@ -1,19 +1,19 @@
 import mongoose from "mongoose";
-// const mongoDB_URL = `mongodb+srv://Amar_Basha:6p8IzICIKu0RiXLs@cluster0.viq6r.mongodb.net/test`;
-import MONGODB_URL from "../config.js"
-  
-    const connectDB = async () => {
-      try {
-        const client = await mongoose.connect(MONGODB_URL, {
-          useNewUrlParser: true,
-          useUnifiedTopology: true,
-        });
-        
-        console.log("MongoDB connected!");
-      } catch (err) {
-        console.error(err.message);
-        process.exit(1);
-      }
+const mongoDB_URL = `mongodb+srv://Amar_Basha:6p8IzICIKu0RiXLs@cluster0.viq6r.mongodb.net/user`;
+// import MONGODB_URL from "../config.js"
+
+const connectDB = async () => {
+  try {
+    const client = await mongoose.connect(mongoDB_URL, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+
+    console.log("MongoDB connected!");
+  } catch (err) {
+    console.error(err.message);
+    process.exit(1);
+  }
 };
-    
+
 export default connectDB;
--- models/user.js
+++ models/user.js
@@ -1,14 +1,13 @@
 import mongoose from "mongoose";
 
-
 const userSchema = new mongoose.Schema({
-  userName: String,
+  userName: { type: String },
   email: { type: String },
-  phone: { type: String,},
-  category: { type: String, },
+  phone: { type: String },
+  category: { type: String },
   status: { type: String },
-  working_place: { type: String, },
-  password: { type: String, },
+  working_place: { type: String },
+  password: { type: String },
 });
 
 const User = mongoose.model("add-user", userSchema);
