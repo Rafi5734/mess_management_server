a010cb68f81a16cd25d7fd66a7d1c7b1a614ea40
Rafi5734 <kamrulhasun5734@gmail.com>
2023-05-09 11:01:03 +0600

    bazarlist api related minor issue solve

--- models/depositList.js
+++ models/depositList.js
@@ -3,9 +3,9 @@ import mongoose from "mongoose";
 const depositListSchema = new mongoose.Schema({
   date: { type: String },
   name: { type: String },
-  deposit_amount: { type: Number },
-  extra_amount: { type: Number },
-  get_amount: { type: Number },
+  deposit_amount: { type: String },
+  extra_amount: { type: String },
+  get_amount: { type: String },
 });
 
 const depositList = mongoose.model("deposit-list", depositListSchema);
